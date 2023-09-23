eaf043a36a1da5afe58af0296e9e7ece0e1cf80e
Lakshminarayanan Ganesan <lakshnarayanan7@gmail.com>
2023-09-23 21:10:50 +0530

    Update playwright.config.js
    Check github actions trigger

--- playwright.config.js
+++ playwright.config.js
@@ -44,7 +44,7 @@ module.exports = defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'],
-    headless:false },
+    headless:true },
     }
 
     /* Test against mobile viewports. */
