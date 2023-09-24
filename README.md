92e8c2bc23d2672a8a5741cf6a2258e6619bb706
Lakshminarayanan Ganesan <lakshnarayanan7@gmail.com>
2023-09-24 19:52:55 +0530

    Refactoring the tests and update Readme

---
+++ README.md
@@ -0,0 +1,51 @@
+# Playwright_JS
+This is a Playwright JavaScript framework for writing API and UI tests. It is designed to be easy to use and extensible, and it provides a number of features to help you write reliable and efficient tests.
+
+# Features
+* Supports both API and UI tests
+* Easy to use and extensible
+* Provides a number of features to help you write reliable and efficient tests, including:
+    * Page objects
+    * Test data management
+    * Reporting
+
+# Getting started
+
+To get started with this framework, you will need to install Node.js and Playwright:
+
+npm install -g node
+npm install playwright
+
+Once you have installed the dependencies, you can clone this repository and install the dependencies for the framework:
+
+```
+git clone https://github.com/LakshminarayananG/PlaywrightJSGM.git
+cd PlaywrightJSGM
+npm install
+```
+
+# Running the tests
+To run the tests, simply execute the following command:
+
+```
+npx playwright test 
+```
+
+This will run all of the tests in the e2eTests folder
+
+# Page objects
+This framework uses page objects to represent the different pages in your application. Page objects are classes that encapsulate the elements and actions on a specific page. This makes it easier to write and maintain your tests, as you can focus on the business logic of your application, rather than the implementation details of the UI.
+
+To create a page object, simply create a new class in the pageobjects folder. The class should extend the BasePageObject class. The BasePageObject class provides a number of common methods that you can use in your page objects, such as methods for finding elements and performing actions.
+
+# Test data management
+This framework provides a test data management system to help you manage the test data for your tests. The test data management system allows you to define test data sets and use them in your tests.
+
+To define a test data set, create a new file in the testdata folder. The file should contain the test data in JSON format. To use a test data set in your test, simply pass the name of the test data set to the testData property of the test function.
+
+# Reporting
+To generate a report, simply execute the following command:
+
+```
+npx playwright show-report
+```
---
+++ e2eTests/E2EUITests.spec.js
@@ -0,0 +1,191 @@
+const { test, expect } = require('@playwright/test');
+const { POBase } = require('../pageobjects/POBase');
+const testData = JSON.parse(JSON.stringify(require('../testdata/inputdata.json')));
+const appURL = "https://demoqa.com/";
+
+test('@Web TC01- Scenario A - Verify user can enter new data into the table', async ({ page }) => {
+
+    const poBase = new POBase(page);
+    const elementsPage = poBase.getElementsPage();
+    const webTablesPage = poBase.getWebTablesPage();
+    const first_name = testData.UserDetails[0].firstName;
+    const last_name = testData.UserDetails[0].lastName;
+    const age = testData.UserDetails[0].age;
+    const userEmail = testData.UserDetails[0].userEmail;
+    const salary = testData.UserDetails[0].salary;
+    const department = testData.UserDetails[0].department;
+
+
+    // Navigate to the demoqa.com website
+    await page.goto(appURL);
+
+    //Navigate to web tables page
+    await elementsPage.navigateToTables();
+
+    //Add details in the web table and click on save
+    await webTablesPage.enterDetailsInWebForm(first_name, last_name, age, userEmail, salary, department);
+
+    //Validate the form is submitted and data details getting reflected
+    const returnedWebTableEntries = await webTablesPage.validateFormSubmissionWithEnteredDetails();
+    const enteredWebTableEntries = [];
+    enteredWebTableEntries.push(testData.UserDetails[0].firstName);
+    enteredWebTableEntries.push(testData.UserDetails[0].lastName);
+    enteredWebTableEntries.push(testData.UserDetails[0].age);
+    enteredWebTableEntries.push(testData.UserDetails[0].userEmail);
+    enteredWebTableEntries.push(testData.UserDetails[0].salary);
+    enteredWebTableEntries.push(testData.UserDetails[0].department);
+
+    //Validation to check if the entered and returned text in webtables are equal
+    expect(enteredWebTableEntries == returnedWebTableEntries);
+
+
+});
+
+test('@Web TC01- Scenario B - Verify user can edit the row in a table', async ({ page }) => {
+
+    const poBase = new POBase(page);
+    const elementsPage = poBase.getElementsPage();
+    const webTablesPage = poBase.getWebTablesPage();
+    const nameToChange = "Alden";
+    const updated_firstname = testData.UserDetails[0].firstName;
+    const updated_lastname = testData.UserDetails[0].lastName;
+
+
+
+    //Launch the browser. Navigating directly to the web tables page
+    await page.goto(appURL);
+    await elementsPage.navigateToTables();
+
+    //Perform name change and validate if changes get reflected
+    await webTablesPage.editTableAndValidateChanges(nameToChange,updated_firstname,updated_lastname)
+   
+  });
+
+test(' @Web Verify broken image', async ({ page }) => {
+
+    const poBase = new POBase(page);
+    const elementsPage = poBase.getElementsPage();
+    const brokenLinksPage = poBase.getBrokenLinksPage();
+
+    // Go to the URL
+    await page.goto(appURL);
+  
+    // Navigate to "Elements" >> "Broken Links-Images"
+    await elementsPage.navigateToBrokenLinks();
+
+    //Validate if the image is broken. Image's dimension will be 0,0 and validating the same from the array received
+    const returnedBrokenImageDimensions = await brokenLinksPage.validateBrokenImage();
+    expect(returnedBrokenImageDimensions[0]).toEqual(0);
+    expect(returnedBrokenImageDimensions[1]).toEqual(0);
+    
+    
+  });
+
+
+test('@Web TC03 - Verify user can submit the form.', async ({ page }) => {
+
+    const poBase = new POBase(page);
+    const formsPage = poBase.getFormsPage();
+    const practiceFormsPage = poBase.getPracticeFormsPage();
+    const firstName = testData.FormDetails[0].firstName;
+    const lastName = testData.FormDetails[0].lastName;
+    const userEmail = testData.FormDetails[0].userEmail;
+    const userNumber =testData.FormDetails[0].userNumber;
+    const dateOfBirth = testData.FormDetails[0].dateOfBirth;
+    const imageName = testData.FormDetails[0].imageName;
+    const currentAddress = testData.FormDetails[0].currentAddress;
+    const confirmationMessage = testData.FormDetails[0].confirmationMessage;
+    const studentName = firstName.concat(" ",lastName);
+
+    // Go to the URL
+    await page.goto(appURL);
+  
+    // Navigate to "Forms" >> "Practice Form"
+    await formsPage.navigateToPracticeForm();
+  
+    
+    // Fill out the practice form with necessary details
+    await practiceFormsPage.fillPracticeForm(firstName,lastName,userEmail,userNumber,dateOfBirth,imageName,currentAddress);
+
+    //Validate the confirmation screen with the details
+    const returnedFormDetailsPostSubmit = await practiceFormsPage.validateFilledPracticeFormData();
+    const filledInputFormDetails = []
+    filledInputFormDetails.push(confirmationMessage);
+    filledInputFormDetails.push(userNumber);
+    filledInputFormDetails.push(studentName);
+
+    //Assertions
+    expect(filledInputFormDetails).toEqual (returnedFormDetailsPostSubmit);
+  
+  
+  });
+
+test('@Web Verify the progress bar', async ({ page }) => {
+    const poBase = new POBase(page);
+    const widgetPage = poBase.getWidgetPage();
+    const progressBarPage = poBase.getProgressBarPage();
+    const validationMessage = testData.ValidationMessages[0].progressBarFinish;
+    
+    // Go to the URL
+    await page.goto(appURL);
+  
+    // Navigate to "Widget" >> "Progress Bar"
+    await widgetPage.navigateToProgressBarPage();
+  
+    // Click on "Start"
+    await progressBarPage.startProgressBar();
+  
+    // Wait for the progress bar to reach 100% and perform assertions
+    expect(await progressBarPage.validateProgressBarCompletion()).toEqual(validationMessage);
+  
+  });
+
+test('@Web Verify the ToolTip', async ({ page }) => {
+    const poBase = new POBase(page);
+    const widgetPage = poBase.getWidgetPage();
+    const toolTipPage = poBase.getToolTipPage();
+    const validationMessage = testData.ValidationMessages[0].LinkHoverText;
+
+    // Go to the URL
+    await page.goto(appURL);
+
+    // Navigate to "Widget" >> "Tool Tip"
+    await widgetPage.navigateToToolTipPage();
+
+    //Hover on the tool tip
+    await toolTipPage.hoverOnToolTip();
+
+    // Wait for the tooltip to appear and validate if text matches
+    expect(await toolTipPage.validateToolTip()).toEqual(validationMessage);
+
+
+  });
+
+test('@Web Verify the Drag & Drop Functionality', async ({ page }) => {
+    const poBase = new POBase(page);
+    const interactionsPage = poBase.getInteractionsPage();
+    const beforeDropTextMessage = testData.ValidationMessages[0].beforeDropText;
+    const afterDropTextMessage = testData.ValidationMessages[0].afterDropText;
+    
+    
+    // Go to the URL
+    await page.goto(appURL);
+  
+    // Navigate to "Interactions" and then "Droppable"
+    await interactionsPage.navigateToDragAndDropPage();
+  
+    // Get the initial text of the "Drop Here" area
+    const initialDropHereText = await interactionsPage.dragAndDropElementText();
+  
+    // Drag the "Drag me" box and drop it onto the "Drop Here" area
+    await interactionsPage.performDragAndDrop();
+  
+    // Get the updated text of the "Drop Here" area
+    const updatedDropHereText = await interactionsPage.dragAndDropElementText();
+  
+    // Assertions
+    expect(initialDropHereText).toBe(beforeDropTextMessage);
+    expect(updatedDropHereText).toBe(afterDropTextMessage);
+  
+  
+  });  
